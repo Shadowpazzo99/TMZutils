0e0cfdb0feed6defaf5b997d53fd97067a11fdc5
GabrielX24 <Gabriele@DESKTOP-0FC9JLK>
2024-01-30 19:34:43 +0100

    armor

--- src/main/java/net/mcreator/tmzutils/init/TmzUtilsModItems.java
+++ src/main/java/net/mcreator/tmzutils/init/TmzUtilsModItems.java
@@ -8,13 +8,22 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.Registry;
 
+import net.mcreator.tmzutils.item.ReinforcedLeatherArmorItem;
 import net.mcreator.tmzutils.item.CrystallizerItem;
 import net.mcreator.tmzutils.TmzUtilsMod;
 
 public class TmzUtilsModItems {
+	public static Item REINFORCED_LEATHER_ARMOR_HELMET;
+	public static Item REINFORCED_LEATHER_ARMOR_CHESTPLATE;
+	public static Item REINFORCED_LEATHER_ARMOR_LEGGINGS;
+	public static Item REINFORCED_LEATHER_ARMOR_BOOTS;
 	public static Item CRYSTALLIZER;
 
 	public static void load() {
+		REINFORCED_LEATHER_ARMOR_HELMET = Registry.register(BuiltInRegistries.ITEM, new ResourceLocation(TmzUtilsMod.MODID, "reinforced_leather_armor_helmet"), new ReinforcedLeatherArmorItem.Helmet());
+		REINFORCED_LEATHER_ARMOR_CHESTPLATE = Registry.register(BuiltInRegistries.ITEM, new ResourceLocation(TmzUtilsMod.MODID, "reinforced_leather_armor_chestplate"), new ReinforcedLeatherArmorItem.Chestplate());
+		REINFORCED_LEATHER_ARMOR_LEGGINGS = Registry.register(BuiltInRegistries.ITEM, new ResourceLocation(TmzUtilsMod.MODID, "reinforced_leather_armor_leggings"), new ReinforcedLeatherArmorItem.Leggings());
+		REINFORCED_LEATHER_ARMOR_BOOTS = Registry.register(BuiltInRegistries.ITEM, new ResourceLocation(TmzUtilsMod.MODID, "reinforced_leather_armor_boots"), new ReinforcedLeatherArmorItem.Boots());
 		CRYSTALLIZER = Registry.register(BuiltInRegistries.ITEM, new ResourceLocation(TmzUtilsMod.MODID, "crystallizer"), new CrystallizerItem());
 	}
 
---
+++ src/main/java/net/mcreator/tmzutils/item/ReinforcedLeatherArmorItem.java
@@ -0,0 +1,99 @@
+
+package net.mcreator.tmzutils.item;
+
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.sounds.SoundEvent;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.fabricmc.api.Environment;
+import net.fabricmc.api.EnvType;
+
+import java.lang.reflect.Type;
+
+public abstract class ReinforcedLeatherArmorItem extends ArmorItem {
+	public ReinforcedLeatherArmorItem(Type type, Item.Properties properties) {
+		super(new ArmorMaterial() {
+			@Override
+			public int getDurabilityForType(Type type) {
+				return new int[]{13, 15, 16, 11}[type.getSlot().getIndex()] * 15;
+			}
+
+			@Override
+			public int getDefenseForType(Type type) {
+				return new int[]{2, 6, 5, 2}[type.getSlot().getIndex()];
+			}
+
+			@Override
+			public int getEnchantmentValue() {
+				return 9;
+			}
+
+			@Override
+			public SoundEvent getEquipSound() {
+				return null;
+			}
+
+			@Override
+			public Ingredient getRepairIngredient() {
+				return Ingredient.of(new ItemStack(Items.LEATHER));
+			}
+
+			@Environment(EnvType.CLIENT)
+			@Override
+			public String getName() {
+				return "reinforcedleather";
+			}
+
+			@Override
+			public float getToughness() {
+				return 0f;
+			}
+
+			@Override
+			public float getKnockbackResistance() {
+				return 0f;
+			}
+		}, type, properties);
+	}
+
+	public static class Helmet extends ReinforcedLeatherArmorItem {
+		public Helmet() {
+			super(Type.HELMET, new Item.Properties());
+			ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.COMBAT).register(content -> content.accept(this));
+		}
+
+	}
+
+	public static class Chestplate extends ReinforcedLeatherArmorItem {
+
+		public Chestplate() {
+			super(Type.CHESTPLATE, new Item.Properties());
+			ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.COMBAT).register(content -> content.accept(this));
+		}
+
+	}
+
+	public static class Leggings extends ReinforcedLeatherArmorItem {
+
+		public Leggings() {
+			super(Type.LEGGINGS, new Item.Properties());
+			ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.COMBAT).register(content -> content.accept(this));
+		}
+
+	}
+
+	public static class Boots extends ReinforcedLeatherArmorItem {
+
+		public Boots() {
+			super(Type.BOOTS, new Item.Properties());
+			ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.COMBAT).register(content -> content.accept(this));
+		}
+
+	}
+}
